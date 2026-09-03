d50659aba5c309feac2b3304aebd1585d8fe11d2
RR-7799 <kannadafilmcuts@gmail.com>
2026-09-03 14:54:59 +0530

    Enable position intelligence drawer

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,8 @@ import DecisionDock from "./components/DecisionDock";
 import RunScanButton from "./components/RunScanButton";
 import LiveStockTable from "./components/LiveStockTable";
 import CoverageBadge from "./components/CoverageBadge";
+import PositionIntelligence from "./components/PositionIntelligence";
 
 export const metadata={title:"Portfolio AI",description:"Personal portfolio tracker"};
 
-export default function RootLayout({children}){return <html lang="en"><body>{children}<RunScanButton /><DecisionDock /><ChangeDock /><AlertDock /><LiveStockTable /><CoverageBadge /></body></html>}
+export default function RootLayout({children}){return <html lang="en"><body>{children}<RunScanButton /><DecisionDock /><ChangeDock /><AlertDock /><LiveStockTable /><CoverageBadge /><PositionIntelligence /></body></html>}
